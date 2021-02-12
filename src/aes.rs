f35f386107bb2ab280ebaf47dfebb0c2cf44e846
Zach Johnson <zachary.m.johnson@colorado.edu>
2021-02-12 14:14:26 -0700

    Added code and tests for AES in CBC mode.

--- src/aes.rs
+++ src/aes.rs
@@ -3,6 +3,7 @@ use aes::cipher::{BlockCipher, NewBlockCipher};
 use aes::Aes128;
 use std::collections::HashSet;
 use crate::converter::bytes_to_hex;
+use crate::xor::xor_bytes;
 
 ///Encrypts plaintext using AES-ECB and the given 16-byte key.
 ///Will panic if key is not 16 bytes or plaintext is not a multiple of 16 bytes.
@@ -21,9 +22,7 @@ pub fn encrypt_ecb(plaintext: &Vec<u8>, key: &Vec<u8>) -> Vec<u8> {
     for i in 0..plaintext.len() / 16 {
         let mut block = GenericArray::clone_from_slice(&plaintext[16*i..16*(i+1)]);
         cipher.encrypt_block(&mut block);
-        for byte in block {
-            ciphertext.push(byte);
-        }
+        ciphertext.append(&mut block.to_vec());
     }
 
     return ciphertext;
@@ -46,9 +45,7 @@ pub fn decrypt_ecb(ciphertext: &Vec<u8>, key: &Vec<u8>) -> Vec<u8> {
     for i in 0..ciphertext.len() / 16 {
         let mut block = GenericArray::clone_from_slice(&ciphertext[16*i..16*(i+1)]);
         cipher.decrypt_block(&mut block);
-        for byte in block {
-            plaintext.push(byte);
-        }
+        plaintext.append(&mut block.to_vec());
     }
 
     return plaintext;
@@ -67,6 +64,54 @@ pub fn detect_ecb(ciphertext: &Vec<u8>) -> bool {
     }
 
     return unique.len() < (ciphertext.len() / 16);
+}
+
+pub fn encrypt_cbc(plaintext: &Vec<u8>, key: &Vec<u8>, iv: &Vec<u8>) -> Vec<u8> {
+    if key.len() != 16 {
+        panic!("Illegal key length {} passed as an AES key!", key.len());
+    }
+    if iv.len() != 16 {
+        panic!("Illegal IV length {} passed as an AES IV!", iv.len());
+    }
+    if plaintext.len() % 16 != 0 {
+        panic!("Partial block of length {} passed for AES encryption!", plaintext.len() % 16);
+    }
+
+    let mut ciphertext: Vec<u8> = vec![];
+    let mut last_block: Vec<u8> = iv.clone();
+
+    for i in 0..plaintext.len() / 16 {
+        let to_encrypt = xor_bytes(&last_block, &plaintext[16*i..16*(i+1)].to_vec());
+        let mut encrypted = encrypt_ecb(&to_encrypt, &key);
+        last_block = encrypted.clone();
+        ciphertext.append(&mut encrypted);
+    }
+
+    return ciphertext;
+}
+
+pub fn decrypt_cbc(ciphertext: &Vec<u8>, key: &Vec<u8>, iv: &Vec<u8>) -> Vec<u8> {
+    if key.len() != 16 {
+        panic!("Illegal key length {} passed as an AES key!", key.len());
+    }
+    if iv.len() != 16 {
+        panic!("Illegal IV length {} passed as an AES IV!", iv.len());
+    }
+    if ciphertext.len() % 16 != 0 {
+        panic!("Partial block of length {} passed for AES decryption!", ciphertext.len() % 16);
+    }
+
+    let mut plaintext: Vec<u8> = vec![];
+    let mut last_block = iv.clone();
+
+    for i in 0..ciphertext.len() / 16 {
+        let decrypted = decrypt_ecb(&ciphertext[16*i..16*(i+1)].to_vec(), &key);
+        let mut plaintext_block = xor_bytes(&last_block, &decrypted);
+        last_block = ciphertext[16*i..16*(i+1)].to_vec();
+        plaintext.append(&mut plaintext_block);
+    }
+
+    return plaintext;
 }
 
 #[cfg(test)]
@@ -143,5 +188,85 @@ mod tests {
     fn test_detect_ecb_not_block() {
         let ciphertext = crate::converter::ascii_to_bytes("AAAAAAAAAAAAA");
         detect_ecb(&ciphertext);
+    }
+
+    #[test]
+    fn test_encrypt_cbc() {
+        let key = crate::converter::hex_to_bytes("56e47a38c5598974bc46903dba290349");
+        let iv = crate::converter::hex_to_bytes("8ce82eefbea0da3c44699ed7db51b7d9");
+        let plaintext = crate::converter::hex_to_bytes("a0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedf");
+        let ciphertext = crate::converter::hex_to_bytes("c30e32ffedc0774e6aff6af0869f71aa0f3af07a9a31a9c684db207eb0ef8e4e35907aa632c3ffdf868bb7b29d3d46ad83ce9f9a102ee99d49a53e87f4c3da55");
+
+        assert_eq!(encrypt_cbc(&plaintext, &key, &iv), ciphertext);
+    }
+
+    #[test]
+    #[should_panic(expected="Illegal key length 13 passed as an AES key!")]
+    fn test_encrypt_cbc_bad_key() {
+        let key = crate::converter::hex_to_bytes("56e47a38c5598974bc46903dba");
+        let iv = crate::converter::hex_to_bytes("8ce82eefbea0da3c44699ed7db51b7d9");
+        let plaintext = crate::converter::hex_to_bytes("6743C3D1519AB4F2CD9A78AB09A511BD");
+
+        encrypt_cbc(&plaintext, &key, &iv);
+    }
+
+    #[test]
+    #[should_panic(expected="Partial block of length 13 passed for AES encryption!")]
+    fn test_encrypt_cbc_bad_block() {
+        let key = crate::converter::hex_to_bytes("56e47a38c5598974bc46903dba290349");
+        let iv = crate::converter::hex_to_bytes("8ce82eefbea0da3c44699ed7db51b7d9");
+        let plaintext = crate::converter::hex_to_bytes("6743C3D1519AB4F2CD9A78AB09");
+
+        encrypt_cbc(&plaintext, &key, &iv);
+    }
+
+    #[test]
+    #[should_panic(expected="Illegal IV length 13 passed as an AES IV!")]
+    fn test_encrypt_cbc_bad_iv() {
+        let key = crate::converter::hex_to_bytes("56e47a38c5598974bc46903dba290349");
+        let iv = crate::converter::hex_to_bytes("8ce82eefbea0da3c44699ed7db");
+        let plaintext = crate::converter::hex_to_bytes("6743C3D1519AB4F2CD9A78AB09A511BD");
+
+        encrypt_cbc(&plaintext, &key, &iv);
+    }
+
+    #[test]
+    fn test_decrypt_cbc() {
+        let key = crate::converter::hex_to_bytes("56e47a38c5598974bc46903dba290349");
+        let iv = crate::converter::hex_to_bytes("8ce82eefbea0da3c44699ed7db51b7d9");
+        let plaintext = crate::converter::hex_to_bytes("a0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedf");
+        let ciphertext = crate::converter::hex_to_bytes("c30e32ffedc0774e6aff6af0869f71aa0f3af07a9a31a9c684db207eb0ef8e4e35907aa632c3ffdf868bb7b29d3d46ad83ce9f9a102ee99d49a53e87f4c3da55");
+
+        assert_eq!(decrypt_cbc(&ciphertext, &key, &iv), plaintext);
+    }
+
+    #[test]
+    #[should_panic(expected="Illegal key length 13 passed as an AES key!")]
+    fn test_decrypt_cbc_bad_key() {
+        let key = crate::converter::hex_to_bytes("56e47a38c5598974bc46903dba");
+        let iv = crate::converter::hex_to_bytes("8ce82eefbea0da3c44699ed7db51b7d9");
+        let ciphertext = crate::converter::hex_to_bytes("6743C3D1519AB4F2CD9A78AB09A511BD");
+
+        decrypt_cbc(&ciphertext, &key, &iv);
+    }
+
+    #[test]
+    #[should_panic(expected="Partial block of length 13 passed for AES decryption!")]
+    fn test_decrypt_cbc_bad_block() {
+        let key = crate::converter::hex_to_bytes("56e47a38c5598974bc46903dba290349");
+        let iv = crate::converter::hex_to_bytes("8ce82eefbea0da3c44699ed7db51b7d9");
+        let ciphertext = crate::converter::hex_to_bytes("6743C3D1519AB4F2CD9A78AB09");
+
+        decrypt_cbc(&ciphertext, &key, &iv);
+    }
+
+    #[test]
+    #[should_panic(expected="Illegal IV length 13 passed as an AES IV!")]
+    fn test_decrypt_cbc_bad_iv() {
+        let key = crate::converter::hex_to_bytes("56e47a38c5598974bc46903dba290349");
+        let iv = crate::converter::hex_to_bytes("8ce82eefbea0da3c44699ed7db");
+        let ciphertext = crate::converter::hex_to_bytes("6743C3D1519AB4F2CD9A78AB09A511BD");
+
+        decrypt_cbc(&ciphertext, &key, &iv);
     }
 }
