c4faeadd1f1192f57ba62cb68f715d73b6ffc400
johnsozm <z.m.johnson@protonmail.com>
2022-07-25 11:16:21 -0600

    Added method to generate MAC signatures

--- src/diffie_hellman.rs
+++ src/diffie_hellman.rs
@@ -3,6 +3,7 @@ use gmp::mpz::Mpz;
 use crate::converter::{bytes_to_hex, hex_to_bytes};
 use crate::hash::Hash;
 use crate::aes::encrypt_cbc;
+use crate::mac::{create_prefix_mac, MAC};
 
 lazy_static! {
     pub static ref DEFAULT_P: Mpz = Mpz::from_str_radix("ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff", 16).unwrap();
@@ -99,6 +100,11 @@ impl DiffieHellman {
     ///Encrypt message using the generated session key and returns (ciphertext, IV)
     pub fn encrypt_message(&self, message: &Vec<u8>) -> (Vec<u8>, Vec<u8>) {
         return (encrypt_cbc(message, &self.aes_key, &IV), IV.clone());
+    }
+
+    ///Generate a prefix MAC using the generated session key
+    pub fn sign_message(&self, message: &Vec<u8>) -> MAC {
+        return create_prefix_mac(&message, &self.aes_key, Hash::SHA256);
     }
 }
 
